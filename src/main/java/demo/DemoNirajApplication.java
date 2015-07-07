6c11f3b391040330645d5d038f1866f30c1d239c
vbhaktha <vbhaktha@qprosol.com>
2015-07-06 21:06:04 -0400

    Update DemoNirajApplication.java
    Commit comment

--- src/main/java/demo/DemoNirajApplication.java
+++ src/main/java/demo/DemoNirajApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoNirajApplication {
-
+// This is a test comment
     public static void main(String[] args) {
         SpringApplication.run(DemoNirajApplication.class, args);
     }
